edd7cb896bb9d787e70d5427bb7027d5a93e2c98
SaurabhS78 <saurabhsangam78@gmail.com>
2020-10-03 17:02:34 +0530

    Updates

---
+++ precache-manifest.31f7651699925d473398abddb6a84525.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a628251fa3487fddd2c5ee1313d9589",
+    "url": "/fpassign/index.html"
+  },
+  {
+    "revision": "7cdf78f0e9df8af3423d",
+    "url": "/fpassign/static/css/main.68213fec.chunk.css"
+  },
+  {
+    "revision": "cb4ae76aebd6990bb0b0",
+    "url": "/fpassign/static/js/2.e1e21ec6.chunk.js"
+  },
+  {
+    "revision": "44b724887c788dd91af1f626e24324fa",
+    "url": "/fpassign/static/js/2.e1e21ec6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cdf78f0e9df8af3423d",
+    "url": "/fpassign/static/js/main.94d06fa0.chunk.js"
+  },
+  {
+    "revision": "27f1ebbffdb7dd25e428",
+    "url": "/fpassign/static/js/runtime-main.64c44abc.js"
+  },
+  {
+    "revision": "1ad36608a6354e2ec19b8a5fb28d096f",
+    "url": "/fpassign/static/media/Asset 15.1ad36608.svg"
+  },
+  {
+    "revision": "91bd7358934c7da091a4177a8ac3e3ab",
+    "url": "/fpassign/static/media/arrowRight.91bd7358.svg"
+  },
+  {
+    "revision": "751f4a5a855e3ca039170bc80c117eec",
+    "url": "/fpassign/static/media/dismiss.751f4a5a.svg"
+  },
+  {
+    "revision": "50d2a94dc66a82eadc99e249c761111d",
+    "url": "/fpassign/static/media/fampaylogo.50d2a94d.svg"
+  },
+  {
+    "revision": "ce84babd7486e104fc26fdc9346dcc2a",
+    "url": "/fpassign/static/media/remind.ce84babd.svg"
+  },
+  {
+    "revision": "40d66f07ced662140adc3f13e764fad1",
+    "url": "/fpassign/static/media/spinWheel.40d66f07.svg"
+  },
+  {
+    "revision": "e94ea0eabfc826e74307d102fe2b151b",
+    "url": "/fpassign/static/media/spinnerPin.e94ea0ea.svg"
+  },
+  {
+    "revision": "b96b4591a0548bdaa79075f4bb0f9d70",
+    "url": "/fpassign/static/media/statusbar.b96b4591.svg"
+  }
+]);
